c778fcc04c706e7dadf1a16c7696f00315acc7dc
Imran Hasan <imranhasan871@gmail.com>
2022-07-18 19:31:47 +0600

    feature added for country and feels like

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const API_KEY = '46ad7457603b9b0104e633e78cd60e16';
  * @description: Event listener for search button
  * @param {event}
  * @returns {void}
- *
  */
 
 const searchTemprature = () => {
@@ -39,8 +38,10 @@ const displayWeather = (temperature) => {
     setInnerText('city', temperature.name);
     setInnerText('temperature', temperature.main.temp);
     setInnerText('condition', temperature.weather[0].main);
+    setInnerText('country', temperature.sys.country);
+    setInnerText('feels-like', temperature.main.feels_like);
     // set weater icon
-    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const url = `https://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
     const imgIcon = document.getElementById('weather-icon');
     imgIcon.setAttribute('src', url);
 }; // end of displayWeather
